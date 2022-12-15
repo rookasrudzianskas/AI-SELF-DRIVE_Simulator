fb0b46c3e9f953af5793029cca3e802a628a2ce2
Rokas <rookas.rudzenskas@gmail.com>
2022-12-15 15:06:16 +0200

    Add arrow key support
    This commit adds support for the arrow keys to control the car.
    
    The arrow keys are more intuitive than the WASD keys, and are more
    commonly used in games.

--- 01_CarDrivingMechanics/controls.js
+++ 01_CarDrivingMechanics/controls.js
@@ -10,7 +10,37 @@ class Controls {
 
     #addKeyboardListeners() {
         document.onkeydown= (e) => {
+            switch (e.key) {
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+        }
 
+        document.onkeyup= (e) => {
+            switch (e.key) {
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
         }
     }
 }
