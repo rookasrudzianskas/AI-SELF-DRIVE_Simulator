5f8832f9e78f7149c61c891920626b792597421e
Rokas <rookas.rudzenskas@gmail.com>
2022-12-15 15:18:34 +0200

    Fix car movement
    The car was not stopping when the user released the up arrow key.
    
    This commit fixes that by adding a check to see if the speed is greater than the friction.
    
    If it is, the speed is set to 0.

--- 01_CarDrivingMechanics/car.js
+++ 01_CarDrivingMechanics/car.js
@@ -35,6 +35,18 @@ class Car {
             this.speed+=this.friction;
         }
 
+        if(Math.abs(this.speed)  > this.friction){
+            this.speed = 0;
+        }
+
+        if(this.controls.left){
+            this.x -= 2;
+        }
+
+        if(this.controls.right) {
+            this.x += 2;
+        }
+
         this.y -= this.speed;
     }
 
