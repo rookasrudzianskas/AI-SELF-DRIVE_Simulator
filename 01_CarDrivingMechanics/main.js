63dc6a4d9e234cc9792b3fb0d0293387af2dac5c
Rokas <rookas.rudzenskas@gmail.com>
2022-12-16 16:31:52 +0200

    Add a canvas to visualize the neural network
    The neural network is now visualized on the right side of the screen.

--- 01_CarDrivingMechanics/main.js
+++ 01_CarDrivingMechanics/main.js
@@ -1,7 +1,12 @@
 const carCanvas = document.getElementById("carCanvas");
 carCanvas.width = 200;
 
-const ctx = carCanvas.getContext("2d");
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 
@@ -17,15 +22,18 @@ function animate() {
     }
     car.update(road.borders, traffic);
     carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
 
-    ctx.save();
-    ctx.translate(0, -car.y + carCanvas.height * 0.7);
-    road.draw(ctx);
+    carCtx.save();
+    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+    road.draw(carCtx);
 
     for(let i = 0; i < traffic.length; i++) {
-        traffic[i].draw(ctx, "red");
+        traffic[i].draw(carCtx, "red");
     }
-    car.draw(ctx, "blue");
-    ctx.restore();
+    car.draw(carCtx, "blue");
+    carCtx.restore();
+
+    Visualizer.drawNetwork(networkCtx, car.brain);
     requestAnimationFrame(animate);
 }
