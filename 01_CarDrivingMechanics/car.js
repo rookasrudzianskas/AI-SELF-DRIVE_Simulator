4c1c5523a9e8a36558ccd46d0836180f0169b4a9
Rokas <rookas.rudzenskas@gmail.com>
2022-12-16 15:49:34 +0200

    Fixes #1
    - Changed the constructor of the Car class to accept the controlType as a parameter
    - Changed the update method of the Car class to accept the traffic as a parameter
    - Changed the update method of the Car class to call the update method of the sensor
    - Changed the draw method of the Car class to call the draw method of the sensor
    - Changed the update method of the TrafficCar class to accept the traffic as a parameter
    - Changed the update method of the TrafficCar class to call the update method of the sensor
    - Changed the draw method of the TrafficCar class to call the draw method of the sensor

--- 01_CarDrivingMechanics/car.js
+++ 01_CarDrivingMechanics/car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height, controlType, maxSpeed=3){
+    constructor(x,y,width,height,controlType,maxSpeed=3){
         this.x=x;
         this.y=y;
         this.width=width;
@@ -10,25 +10,33 @@ class Car{
         this.maxSpeed=maxSpeed;
         this.friction=0.05;
         this.angle=0;
-        this.damaged = false;
+        this.damaged=false;
 
-
-        this.sensor = new Sensor(this);
+        if(controlType!="DUMMY"){
+            this.sensor=new Sensor(this);
+        }
         this.controls=new Controls(controlType);
     }
 
-    update(roadBorders){
-        if(!this.damaged) {
+    update(roadBorders,traffic){
+        if(!this.damaged){
             this.#move();
-            this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.polygon=this.#createPolygon();
+            this.damaged=this.#assessDamage(roadBorders,traffic);
         }
-        this.sensor.update(roadBorders);
+        if(this.sensor){
+            this.sensor.update(roadBorders,traffic);
+        }
     }
 
-    #assessDamage(roadBorders){
+    #assessDamage(roadBorders,traffic){
         for(let i=0;i<roadBorders.length;i++){
             if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
                 return true;
             }
         }
@@ -36,9 +44,9 @@ class Car{
     }
 
     #createPolygon(){
-        const points = [];
-        const rad = Math.hypot(this.width, this.height) / 2;
-        const alpha = Math.atan2(this.width, this.height);
+        const points=[];
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
         points.push({
             x:this.x-Math.sin(this.angle-alpha)*rad,
             y:this.y-Math.cos(this.angle-alpha)*rad
@@ -62,7 +70,6 @@ class Car{
         if(this.controls.forward){
             this.speed+=this.acceleration;
         }
-
         if(this.controls.reverse){
             this.speed-=this.acceleration;
         }
@@ -70,7 +77,6 @@ class Car{
         if(this.speed>this.maxSpeed){
             this.speed=this.maxSpeed;
         }
-
         if(this.speed<-this.maxSpeed/2){
             this.speed=-this.maxSpeed/2;
         }
@@ -78,22 +84,20 @@ class Car{
         if(this.speed>0){
             this.speed-=this.friction;
         }
-
         if(this.speed<0){
             this.speed+=this.friction;
         }
-
         if(Math.abs(this.speed)<this.friction){
             this.speed=0;
         }
 
-        if(this.speed != 0){
+        if(this.speed!=0){
             const flip=this.speed>0?1:-1;
             if(this.controls.left){
-                this.angle += 0.03*flip;
+                this.angle+=0.03*flip;
             }
             if(this.controls.right){
-                this.angle -= 0.03*flip;
+                this.angle-=0.03*flip;
             }
         }
 
@@ -101,19 +105,21 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx){
-        if(this.damaged) {
-            ctx.fillStyle = "gray";
-        } else {
-            ctx.fillStyle = "black";
+    draw(ctx,color){
+        if(this.damaged){
+            ctx.fillStyle="gray";
+        }else{
+            ctx.fillStyle=color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
-        for(let i = 1; i < this.polygon.length; i++){
+        for(let i=1;i<this.polygon.length;i++){
             ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if(this.sensor){
+            this.sensor.draw(ctx);
+        }
     }
 }
