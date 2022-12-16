8b5022703b53aefef9f467a127daaf7468961080
Rokas <rookas.rudzenskas@gmail.com>
2022-12-16 13:52:55 +0200

    Add a newline after the end of the if statement
    This is a good practice to avoid confusion when reading the code.

--- 01_CarDrivingMechanics/sensor.js
+++ 01_CarDrivingMechanics/sensor.js
@@ -66,6 +66,7 @@ class Sensor{
     draw(ctx){
         for(let i=0;i<this.rayCount;i++){
             let end=this.rays[i][1];
+
             if(this.readings[i]){
                 end=this.readings[i];
             }
