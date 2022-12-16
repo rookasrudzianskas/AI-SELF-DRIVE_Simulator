0fe70bc2b8beaf33e4da5ebe9484a3335bdda9d8
Rokas <rookas.rudzenskas@gmail.com>
2022-12-16 17:00:44 +0200

    Add random colors to traffic cars
    This makes it easier to distinguish between the different cars.

--- 01_CarDrivingMechanics/main.js
+++ 01_CarDrivingMechanics/main.js
@@ -8,7 +8,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=1;
+const N=100;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -22,13 +22,13 @@ if(localStorage.getItem("bestBrain")){
 }
 
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2,getRandomColor()),
 ];
 
 animate();
@@ -54,11 +54,9 @@ function animate(time){
     for(let i=0;i<traffic.length;i++){
         traffic[i].update(road.borders,[]);
     }
-
     for(let i=0;i<cars.length;i++){
         cars[i].update(road.borders,traffic);
     }
-
     bestCar=cars.find(
         c=>c.y==Math.min(
             ...cars.map(c=>c.y)
@@ -72,14 +70,14 @@ function animate(time){
 
     road.draw(carCtx);
     for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(carCtx,"red");
+        traffic[i].draw(carCtx);
     }
     carCtx.globalAlpha=0.2;
     for(let i=0;i<cars.length;i++){
-        cars[i].draw(carCtx,"blue");
+        cars[i].draw(carCtx);
     }
     carCtx.globalAlpha=1;
-    bestCar.draw(carCtx,"blue",true);
+    bestCar.draw(carCtx,true);
 
     carCtx.restore();
 
