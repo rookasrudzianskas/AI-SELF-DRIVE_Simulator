b36acaf288f57de7ed9a3b1291657f9fde04d32b
Rokas <rookas.rudzenskas@gmail.com>
2022-12-16 14:03:10 +0200

    Fix car collision detection
    The car was not detecting collisions with the road borders.
    This commit fixes that by creating a polygon for the car
    and checking if it intersects with any of the road borders.

--- 01_CarDrivingMechanics/car.js
+++ 01_CarDrivingMechanics/car.js
@@ -10,6 +10,7 @@ class Car{
         this.maxSpeed=3;
         this.friction=0.05;
         this.angle=0;
+        this.damaged = false;
 
 
         this.sensor = new Sensor(this);
@@ -18,7 +19,18 @@ class Car{
 
     update(roadBorders){
         this.#move();
+        this.polygon = this.#createPolygon();
+        this.damaged = this.#assessDamage(roadBorders);
         this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
+                return true;
+            }
+        }
+        return false;
     }
 
     #createPolygon(){
@@ -88,20 +100,12 @@ class Car{
     }
 
     draw(ctx){
-        ctx.save();
-        ctx.translate(this.x,this.y);
-        ctx.rotate(-this.angle);
-
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
+        for(let i = 1; i < this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
+        }
         ctx.fill();
-
-        ctx.restore();
 
         this.sensor.draw(ctx);
     }
